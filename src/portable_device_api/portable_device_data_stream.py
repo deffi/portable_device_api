d294b94fa148ab448e65140052f4e8d7da0c0c5b
Martin Herrmann <martin@famherrmann.de>
2024-01-16 20:32:51 +0100

    Clarification

--- src/portable_device_api/portable_device_data_stream.py
+++ src/portable_device_api/portable_device_data_stream.py
@@ -16,10 +16,13 @@ class PortableDeviceDataStream(ComWrapper):
         # noinspection PyTypeChecker,PyCallingNonCallable
         buffer = (ctypes.c_ubyte * chunk_size)()
         # Patched, see portable_device_api._api.portable_device_api
-        # Note that print(self.p) gives POINTER(IStream), but it does seem to
+        # Note that print(self.p) gives POINTER(IPortableDeviceDataStream) (or
+        # POINTER(IStream) if we don't QueryInterface it), but it does seem to
         # be using ISequentialStream - if
         # _1F001332_1A57_4934_BE31_AFFC99F4EE0A_0_1_0.ISequentialStream is
         # modified, then this fails.
+        print()
+        print(self.p)
         _, length = self.p.RemoteRead(
             pv = buffer,
             cb = ctypes.c_ulong(chunk_size))
